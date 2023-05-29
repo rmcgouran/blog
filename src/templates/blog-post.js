b0bf14edfb563693903b0acfe8f1262df2539e21
Ryan McGouran <rmcgouran6@gmail.com>
2023-05-29 14:05:55 +0100

    removed author name
    Author name showing as 'Joe Doe' for some reason - only one that will be publishing to this blog so removing.

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -47,7 +47,7 @@ class BlogPostTemplate extends React.Component {
         />
         <div className={styles.container}>
           <span className={styles.meta}>
-            {post.author?.name} &middot;{' '}
+            {/* {post.author?.name} &middot;{' '} */}
             <time dateTime={post.rawDate}>{post.publishDate}</time> –{' '}
             {timeToRead} minute read
           </span>
